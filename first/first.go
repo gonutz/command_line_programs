40f81b1a38e956f08bf2ebe2412fa5946bc530f0
lars <lars.scheme@gmail.com>
2021-03-19 09:58:34 +0100

    first: handle wrong number of arguments

--- first/first.go
+++ first/first.go
@@ -7,14 +7,17 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) == 1 {
 		io.Copy(os.Stdout, os.Stdin)
 		return
+	} else if len(os.Args) == 2 {
+		n, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			io.Copy(os.Stdout, os.Stdin)
+			return
+		}
+		io.CopyN(os.Stdout, os.Stdin, int64(n))
+	} else {
+		panic("first: too many arguments")
 	}
-	n, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		io.Copy(os.Stdout, os.Stdin)
-		return
-	}
-	io.CopyN(os.Stdout, os.Stdin, int64(n))
 }
